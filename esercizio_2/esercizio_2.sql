23c72796843069c027d811672598e7154ee95d2e
valentinarizzo21 <rizzo.val3ntina@gmail.com>
2025-02-25 14:53:09 +0100

    Add files via upload

---
+++ esercizio_2/esercizio_2.sql
@@ -0,0 +1,9 @@
+SELECT * FROM Products;
+--Select * from Products where UnitsInStock >= 40;
+--select * from Employees where City = 'London';
+--select * from Orders order by Freight desc;
+--select * from Orders where Freight >=90 and Freight <=200;
+--select * from Products where CategoryID = '1';
+--select * from [Order Details] where Discount > 0;
+--select * from Orders where CustomerID = 'BOTTOM' and Freight > 50;
+
